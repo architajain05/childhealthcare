eff3d1db4137dfa0f54fa21d70f119f0b6a70c4e
Archita <122664337+architajain05@users.noreply.github.com>
2023-03-26 22:02:32 +0530

    Create vaccines.py

---
+++ vaccines.py
@@ -0,0 +1,136 @@
+from tkinter import *
+root1=Tk()
+root1.title("Vaccines Screen")
+root1.geometry("800x900")
+root1.attributes("-fullscreen",True)
+root1.configure(bg="salmon")
+
+
+#Birth
+l1=Label(root1,text="Birth",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l1.place(x=40,y=20)
+l2=Label(root1,text="Hepatitis B-1(1 of 4)",font=("Lucida Handwriting",10))
+l3=Label(root1,text="BCG(1 of 1)",font=("Lucida Handwriting",10))
+l4=Label(root1,text="Oral polio vaccine(OPV)(1 of 1)",font=("Lucida Handwriting",10))
+l2.place(x=40,y=60)
+l3.place(x=40,y=80)
+l4.place(x=40,y=100)
+#6 Weeks
+l5=Label(root1,text="6 Weeks",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l5.place(x=40,y=140)
+l6=Label(root1,text="Hepatitis B-2(2 of 4)",font=("Lucida Handwriting",10))
+l7=Label(root1,text="DTWP(1 of 3)",font=("Lucida Handwriting",10))
+l8=Label(root1,text="Rotavirus(1 of 3)",font=("Lucida Handwriting",10))
+l9=Label(root1,text="Penta-1",font=("Lucida Handwriting",10))
+l10=Label(root1,text="PCV",font=("Lucida Handwriting",10))
+l6.place(x=40,y=180)
+l7.place(x=40,y=200)
+l8.place(x=40,y=220)
+l9.place(x=40,y=240)
+l10.place(x=40,y=260)
+#10 Weeks
+l11=Label(root1,text="10 Weeks",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l11.place(x=40,y=300)
+l12=Label(root1,text="Hepatitis B-3(3 of 4)",font=("Lucida Handwriting",10))
+l13=Label(root1,text="DTWP(2 of 3)",font=("Lucida Handwriting",10))
+l14=Label(root1,text="Rotavirus(2 of 3)",font=("Lucida Handwriting",10))
+l15=Label(root1,text="PCV",font=("Lucida Handwriting",10))
+l16=Label(root1,text="Penta-2",font=("Lucida Handwriting",10))
+l12.place(x=40,y=340)
+l13.place(x=40,y=360)
+l14.place(x=40,y=380)
+l15.place(x=40,y=400)
+l16.place(x=40,y=420)
+
+#14 Weeks
+l17=Label(root1,text="14 Weeks",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l17.place(x=40,y=460)
+l18=Label(root1,text="Hepatitis B-4(4 of 4)",font=("Lucida Handwriting",10))
+l19=Label(root1,text="DTWP(3 of 3)",font=("Lucida Handwriting",10))
+l20=Label(root1,text="Rotavirus(3 of 3)",font=("Lucida Handwriting",10))
+l21=Label(root1,text="Penta-3",font=("Lucida Handwriting",10))
+l22=Label(root1,text="PCV",font=("Lucida Handwriting",10))
+l17.place(x=40,y=540)
+l18.place(x=40,y=560)
+l19.place(x=40,y=580)
+l20.place(x=40,y=600)
+l21.place(x=40,y=620)
+l22.place(x=40,y=640)
+
+#6 Months
+l23=Label(root1,text="6 Months",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l23.place(x=400,y=40)
+l24=Label(root1,text="Influenza-1(1 of 2)",font=("Lucida Handwriting",10))
+l25=Label(root1,text="Vitamin-A",font=("Lucida Handwriting",10))
+
+l24.place(x=400,y=80)
+l25.place(x=400,y=100)
+
+#7 Months
+l26=Label(root1,text="7 Months",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l26.place(x=400,y=140)
+l27=Label(root1,text="Influenza-2(2 of 2)",font=("Lucida Handwriting",10))
+
+l27.place(x=400,y=180)
+
+#9 Months
+l28=Label(root1,text="9 Months",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l28.place(x=400,y=220)
+l29=Label(root1,text="Yellow Fever(1 of 1)",font=("Lucida Handwriting",10))
+l29.place(x=400,y=260)
+l30=Label(root1,text="Typhoid Conjugate(1 of 1)",font=("Lucida Handwriting",10))
+l30.place(x=400,y=280)
+
+#12 Months
+l31=Label(root1,text="12 Months",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l31.place(x=400,y=320)
+l32=Label(root1,text="Hepatitis A-1(1 of 2)",font=("Lucida Handwriting",10))
+l33=Label(root1,text="Cholera(1 of 1)",font=("Lucida Handwriting",10))
+l34=Label(root1,text="Japanese Encephalitis(1 of 1)",font=("Lucida Handwriting",10))
+l32.place(x=400,y=360)
+l33.place(x=400,y=380)
+l34.place(x=400,y=400)
+
+##15 Months
+l35=Label(root1,text="15 Months",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l35.place(x=400,y=440)
+l36=Label(root1,text="Hepatitis A-1(1 of 2)",font=("Lucida Handwriting",10))
+l37=Label(root1,text="Cholera(1 of 1)",font=("Lucida Handwriting",10))
+l38=Label(root1,text="Japanese Encephalitis(1 of 1)",font=("Lucida Handwriting",10))
+l36.place(x=400,y=480)
+l37.place(x=400,y=500)
+l38.place(x=400,y=520)
+
+
+##16-18 Months
+
+l39=Label(root1,text="16-18 Months",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l39.place(x=800,y=40)
+l40=Label(root1,text="DTWP(Booster)",font=("Lucida Handwriting",10))
+l41=Label(root1,text="H.I.B(Booster)",font=("Lucida Handwriting",10))
+l40.place(x=800,y=80)
+l41.place(x=800,y=100)
+
+##18-24 Months
+l42=Label(root1,text="18-24 Months",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l42.place(x=800,y=140)
+l43=Label(root1,text="Typhoid Conjugate Vaccine(Booster)",font=("Lucida Handwriting",10))
+l43.place(x=800,y=180)
+
+##4-6 Years
+l44=Label(root1,text="4-6 Years",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l44.place(x=800,y=230)
+l45=Label(root1,text="DTWP(Booster)",font=("Lucida Handwriting",14))
+l46=Label(root1,text="Varicella(Booster)",font=("Lucida Handwriting",14))
+l45.place(x=800,y=270)
+l46.place(x=800,y=300)
+
+##10 Years
+l47=Label(root1,text="10 Years",font=("Lucida Handwriting",15),bg="pink",fg="blue")
+l47.place(x=800,y=340)
+l48=Label(root1,text="Tdap/Td",font=("Lucida Handwriting",10))
+l49=Label(root1,text="HPV(Human Papilloma Virus)(Girls Only)",font=("Lucida Handwriting",10))
+l48.place(x=800,y=380)
+l49.place(x=800,y=400)
+
+root1.mainloop()
